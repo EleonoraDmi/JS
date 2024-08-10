22bb69558822c19b838729c19e9df7c21030e1bb
EleonoraDmi <eleonora.dmitrenco@gmail.com>
2024-08-10 17:43:57 +0300

    Add files via upload

---
+++ Script_BMI.js
@@ -0,0 +1,27 @@
+// T/a_Varianta 1
+let weightVadim = 78;
+let heightVadim = 1.69;
+let bmiVadim = weightVadim / heightVadim **2;
+console.log(bmiVadim.toFixed(2))
+
+let weightAlex = 92;
+let heightAlex = 1.95;
+let bmiAlex = weightAlex / heightAlex **2;
+console.log(bmiAlex.toFixed(2))
+
+let markHigherBMI = bmiVadim > bmiAlex
+console.log(markHigherBMI)
+
+// T/a_Varianta 2
+let weightVadim = 95;
+let heightVadim = 1.88;
+let bmiVadim = weightVadim / heightVadim **2;
+console.log(bmiVadim.toFixed(2))
+
+let weightAlex = 85;
+let heightAlex = 1.76;
+let bmiAlex = weightAlex / heightAlex **2;
+console.log(bmiAlex.toFixed(2))
+
+let markHigherBMI = bmiVadim > bmiAlex
+console.log(markHigherBMI)
